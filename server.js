a8223b4e0b875e3a8fbe5f01867629859a988875
Addis Negash <anegash154@gmail.com>
2026-03-04 14:28:01 +0300

    feat: create a server which listen at --- port

---
+++ server.js
@@ -0,0 +1,8 @@
+const http=require('http');
+const{userRoutes}=require('./routes/userRoutes.js');
+const server=http.createServer(async (req,res)=>{
+    await userRoutes(req,res);
+});
+server.listen(8080,()=>{
+    console.log(`server is listening at port 8080`);
+});
