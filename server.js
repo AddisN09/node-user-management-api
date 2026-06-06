9104ee6210bca839a5d81dad9111cbc9c27ac4b8
Addis Negash <anegash154@gmail.com>
2026-06-06 13:32:57 +0300

    feat : start the server and the database

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 require('dotenv').config();
-const http=require('http');
-const{userRoutes}=require('./routes/userRoutes.js');
-const server=http.createServer(async (req,res)=>{
-    await userRoutes(req,res);
-});
-server.listen(8080,()=>{
-    console.log(`server is listening at port 8080`);
-});
+const express=require('express');
+const connectDB=require('./config/db.js');
+const {router}=require('./routes/userRoutes.js');
+
+const app=express();
+
+app.use(express.json());
+app.use('/users',router);
+
+let connect;
+
+async function start(){
+    connect=await connectDB();
+    app.locals.connect=connect;
+    let port=process.env.port||3000;
+    app.listen(port,()=>{
+        console.log(`server is running at port ${port}. To stop press ctrl+c`);
+    });
+}
+start();
+
+
+
+
